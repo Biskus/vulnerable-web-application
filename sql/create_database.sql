87320db5c91de9f9b60a22b28e16c86af6cda74b
Biskus <biskus@gmail.com>
2021-03-15 21:50:27 +0100

    Added uke 11 databaseoppgaver + import av skoler i norge

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -2,7 +2,7 @@ create database task5;
 
 use task5;
 
-create table if not exists users(
+CREATE TABLE IF NOT EXISTS users(
     username varchar(255) primary key not null,
     password varchar(255) not null
 );
